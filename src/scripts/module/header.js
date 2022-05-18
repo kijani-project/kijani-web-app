503d6c0e57ec7cbec9e63a6c2f579bf4025e9343
Mathias Reker <mathias@reker.dk>
2022-05-18 18:28:36 +0200

    header links

---
+++ src/scripts/module/header.js
@@ -0,0 +1,35 @@
+import {HttpClient} from "src/scripts/module/HttpClient";
+
+
+async function updateDropdownMenuCategories() {
+  const categoryEndpoint = restApi + "/categories";
+  const categoryUl = document.querySelector('[aria-labelledby="navbarDropdownMenuCategories"]');
+  const pageName = "kategori";
+  const productHtmlPage = "produkter.html";
+  const categories = await new HttpClient(categoryEndpoint).get();
+
+  Object.values(categories).forEach(el => {
+    let url = new URL(domain + "/" + productHtmlPage);
+    url.searchParams.append(pageName, el.categoryId)
+    categoryUl.innerHTML += `<li><a class="dropdown-item" href="${url}">${el.categoryName}</a></li>`;
+  })
+}
+
+async function updateDropdownMenuProductEcoLabels() {
+  const productEcoLabelsEndpoint = restApi + "/productEcolabels";
+  const categoryUl = document.querySelector('[aria-labelledby="navbarDropdownMenuProductEcoLabels"]');
+  const pageName = "miljomarke";
+  const productHtmlPage = "produkter.html";
+  const categories = await new HttpClient(productEcoLabelsEndpoint).get();
+
+  Object.values(categories).forEach(el => {
+    let url = new URL(domain + "/" + productHtmlPage);
+    url.searchParams.append(pageName, el.productEcolabelId)
+    categoryUl.innerHTML += `<li><a class="dropdown-item" href="${url}">${el.productEcolabelName}</a></li>`;
+  })
+}
+
+window.addEventListener("load", async () => {
+  await updateDropdownMenuCategories();
+  await updateDropdownMenuProductEcoLabels();
+});
