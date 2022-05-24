aebd2de8af0d81f953480ea88064128dd5edd298
Christopher Duran  Nielsen <chri42fk@stud.kea.dk>
2022-05-24 09:48:06 +0200

    Minor changes so it fetches more data and styling

--- src/scripts/module/product-info.js
+++ src/scripts/module/product-info.js
@@ -5,37 +5,42 @@ const productParam = url.searchParams.get("productId");
 const productEndpoint = restApi + "/products";
 
 
-
-
 async function getProduct() {
   const product = await new HttpClient(productEndpoint + "/" + productParam).get();
 
   console.log(product);
 
   let name = product.name;
-  let image = product.imageLink;
   let desc = product.description;
- // let designer = Object.values(product.designers.name).join(", ");
-  let ecolabel = product.productEcoLabels.type;
+  let designers = [];
+  Object.values(product.designers).forEach(designer => {designers.push (designer.name)});
+  //let ecolabel = product.productEcoLabels[0].type;
   let width = product.measurement.width;
   let length= product.measurement.length;
   let height = product.measurement.height;
-  document.getElementById("product-name").innerText=  name;
-  document.getElementById("product-header").innerText=  name;
-  document.getElementById("product-desc").innerText=  desc;
-  //document.getElementById("product-designers").innerText=  designer;
-  document.getElementById("product-ecolabel").innerText=  ecolabel;
-  document.getElementById("product-mesurement-w").innerText=  width;
-  document.getElementById("product-mesurement-l").innerText=  length;
-  document.getElementById("product-mesurement-h").innerText=  height;
-
+  document.getElementById("product-name").innerText= name;
+  document.getElementById("product-header").innerText= name;
+  document.getElementById("product-desc").innerText= desc;
+  document.getElementById("product-designers").innerText= designers.join(", ");
+  //let productEcolabel= document.getElementById("product-ecolabel");
+  document.getElementById("product-measurement-w").innerText= width;
+  document.getElementById("product-measurement-l").innerText= length;
+  document.getElementById("product-measurement-h").innerText= height;
+  let productImage = document.getElementById("product-image");
+  productImage.setAttribute("src",product.imageLink)
+  //productEcolabel.innerHTML=`<span>${product.productEcoLabels[0].type} </span><img src="${product.productEcoLabels[0].imageLink}">`
+  //let ecolabelImg = document.getElementById("product-ecolabelImg");
+  //ecolabelImg.setAttribute("src",product.productEcoLabels[0].imageLink)
+  let ecolabelImg= [];
+  Object.values(product.productEcoLabels.type).forEach(ecolabel => {ecolabelImg.push (ecolabel.type)});
+  document.getElementById("product-ecolabelImg").innerText= ecolabel.join(", ");
 
 
 
   console.log(name);
-  console.log(image);
-  console.log(desc);
-  console.log(ecolabel + "ecolabel");
+  console.log(ecolabelImg);
+  console.log(ecolabel + "= ecolabel");
+
 }
 window.addEventListener("load",async () => {
   await getProduct();
